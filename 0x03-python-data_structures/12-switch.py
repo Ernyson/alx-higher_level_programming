46eb83838fd9ab1fafb712fdd12c619d5bde1bf7
Ernyson <ernestaiji1@gmail.com>
2022-08-29 23:53:40 +0000

    edited file

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,18 +1,5 @@
-#include "lists.h"
-/**
- * reverse_listint - Reverses a singly-linked listint_t list.
- * @head: A pointer to the starting node of the list to reverse.
- *
- * Return: A pointer to the head of the reversed list.
- */
-listint_t *reverse_listint(listint_t **head)
-{
-	listint_t *node = *head, *next, *prev = NULL;
-
-while (node)
-	{
-		next = node->next;
-		node->next = prev;
-		prev = node;
-		node = next;
-	}
+#!/usr/bin/python3
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
