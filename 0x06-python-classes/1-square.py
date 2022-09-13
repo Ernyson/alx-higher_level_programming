99feeacad34cb06604feb20cb9ba80ccc3cbc4ab
Ernyson <ernestaiji1@gmail.com>
2022-09-13 17:11:25 +0000

    new file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size):
+        """ private instance attribute """
+        self.__size = size
+
+
+if __name__ == "__main__":
+    Square()
