faf9ee7e8c781b6664c50b57db0f6b6ad7a2f5a4
Ernyson <ernestaiji1@gmail.com>
2022-09-16 12:30:06 +0000

    Print Square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+
+"""Define a class Square"""
 
 
 class Square:
-    """Represent a square."""
-
+    """Square class"""
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-            position (int, int): The position of the new square.
-        """
+        """Initialize a new class"""
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
+        """Getter for square size."""
         return (self.__size)
-1
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -29,7 +25,7 @@ class Square:
 
     @property
     def position(self):
-        """Get/set the current position of the square."""
+        """Getter for square position."""
         return (self.__position)
 
     @position.setter
@@ -37,16 +33,16 @@ class Square:
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):0
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        """Return the current area of the square."""
+        """Area of square"""
         return (self.__size * self.__size)
 
     def my_print(self):
-        """Print the square with the # character."""
+        """Print area of a square"""
         if self.__size == 0:
             print("")
             return
@@ -58,7 +54,7 @@ class Square:
             print("")
 
     def __str__(self):
-        """Define the print() representation of a Square."""
+        """ print() representation of a Square."""
         if self.__size != 0:
             [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
@@ -67,3 +63,7 @@ class Square:
             if i != self.__size - 1:
                 print("")
         return ("")
+
+
+if __name__ == "__main__":
+    Square()
