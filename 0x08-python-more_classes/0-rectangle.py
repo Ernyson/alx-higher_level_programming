a3839be070439f7014c3a7b9aa3ad71dd48bd8dd
Ernyson <ernestaiji1@gmail.com>
2022-09-19 15:49:27 +0000

    Write an empty class Rectangle that defines a rectangle: not allowed to import module"

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Define a class Rectangle"""
+
+
+class Rectangle:
+    """Empty Class"""
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    Rectangle()
