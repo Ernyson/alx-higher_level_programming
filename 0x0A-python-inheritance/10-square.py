d6cb045e5fa4866d5e67d5b9c0c275f5c2b69a1d
Ernyson <ernestaiji1@gmail.com>
2022-09-27 00:02:20 +0000

    A class Square that inherits from Rectangle (9-rectangle.py)

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
 """
-Contains class MyInt that inherits from int
-Sneaky --> has == and != operators inverted!
+Contains the class BaseGeometry and subclass Rectangle
 """
 
+Rectangle = __import__('9-rectangle').Rectangle
 
-class MyInt(int):
-    """
-    Methods:
-        __init__(self, num)
-        __eq__(self, other)
-        __ne__(self, other)
-    """
-    def __init__(self, num):
-        """initialize num"""
-        self.num = num
 
-    def __eq__(self, other):
-        """
-        Return:
-           True if both not equal
-        """
-        return self.num != other
+class Square(Rectangle):
+    """A representation of a square"""
+    def __init__(self, size):
+        """instantiation of the square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
-    def __ne__(self, other):
-        """
-        Return:
-           True if both equal
-        """
-        return self.num == other
+    def area(self):
+        """"returns the area of the square"""
+        return self.__size ** 2
